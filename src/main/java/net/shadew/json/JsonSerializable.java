ec280f79168f5b06367a77853aa7405c09b91dc4
ShadewRG <onankman@gmail.com>
2021-12-19 12:51:00 +0100

    Type system start

--- src/main/java/net/shadew/json/JsonSerializable.java
+++ src/main/java/net/shadew/json/JsonSerializable.java
@@ -1,5 +1,9 @@
 package net.shadew.json;
 
+/**
+ * @deprecated In favour of the new type serializer API
+ */
+@Deprecated
 public interface JsonSerializable {
     JsonNode toJsonTree();
 }
---
+++ src/main/java/net/shadew/json/type/TypeRegistry.java
@@ -0,0 +1,4 @@
+package net.shadew.json.type;
+
+public class TypeRegistry {
+}
